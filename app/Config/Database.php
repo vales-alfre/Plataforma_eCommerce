bfde92998a845e82a3764b8ed8873f55bdf25b02
DELL <cristian_uteq@hotmail.com>
2024-07-12 11:15:13 -0500

    New parameters

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => '',
+        'password'     => 'Uteq2024',
         'database'     => 'tienda',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',

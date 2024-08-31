9e2520a548c01afd21be4a5cc6e9774ad154f7aa
DELL <cristian_uteq@hotmail.com>
2024-08-31 09:40:49 -0500

    NewBaseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/tienda/';
+    public string $baseURL = 'http://tiendabiu.us-east-1.elasticbeanstalk.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
